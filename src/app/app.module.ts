c155bf823588736fb7dfe93781d3763c5a4f8082
lucasabdouni <lucasabdouni@hotmail.com>
2024-02-03 19:20:07 -0300

    feat: created weather services for api calls and create function for subscription on home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherHomeComponent } from './modules/wheater/page/weather-home/weather-home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WeatherHomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherDatas } from 'src/app/models/interfaces/weather.interface';
+import { WeatherService } from '../../services/weather.service';
+
+@Component({
+  selector: 'app-weather-home',
+  templateUrl: './weather-home.component.html',
+  styleUrls: [],
+})
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = 'São Paulo';
+  weatherDatas!: WeatherDatas;
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName).subscribe({
+      next: (response) => {
+        response && (this.weatherDatas = response);
+      },
+      error: (error) => console.log(error),
+    });
+  }
+}
