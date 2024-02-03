3f7592e19e656210dd1af2c9bc32a35e5a02fb45
lucasabdouni <lucasabdouni@hotmail.com>
2024-02-03 19:53:45 -0300

    feat: created input for research

--- src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { Subject, takeUntil } from 'rxjs';
 import { WeatherDatas } from 'src/app/models/interfaces/weather.interface';
 import { WeatherService } from '../../services/weather.service';
 
@@ -7,22 +9,35 @@ import { WeatherService } from '../../services/weather.service';
   templateUrl: './weather-home.component.html',
   styleUrls: [],
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
   initialCityName = 'São Paulo';
   weatherDatas!: WeatherDatas;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
     this.getWeatherDatas(this.initialCityName);
   }
-
   getWeatherDatas(cityName: string): void {
-    this.weatherService.getWeatherDatas(cityName).subscribe({
-      next: (response) => {
-        response && (this.weatherDatas = response);
-      },
-      error: (error) => console.log(error),
-    });
+    this.weatherService
+      .getWeatherDatas(cityName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          response && (this.weatherDatas = response);
+          console.log(this.weatherDatas);
+        },
+        error: (error) => console.log(error),
+      });
+  }
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    this.initialCityName = '';
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
